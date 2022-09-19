76be0c9db0a2a1ac047319212a6017ab0a356773
arshad292 <arshad.kma@gmail.com>
2022-09-19 22:49:02 +0530

    update

--- calc.py
+++ calc.py
@@ -1,3 +1,51 @@
 import tkinter as tk
 
+LARGE_FONT_STYEL = ("Arial", 40,"bold")
+SMALL_FONT_STYLE = ("Arial", 16)
+LIGHT_GRAY = "#F5F5F5"
+LABEL_COLOR = "#25265E"
+
+
 class Calculator:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("375x667")
+        self.window.resizable(0, 0)
+        self.window.title("Calculator")
+        self.total_expression = "0"
+        self.current_expression = "0"
+        self.display_frame = self.create_display_frame()
+
+
+        self.display_frame = self.create_display_frame()
+
+    def create_display_labels(self):
+        totsl_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                               fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
+
+        totsl_label.pack(expand=True, fill='both')
+
+        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                               fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYEL)
+
+        label.pack(expand=True, fill='both')
+
+        return totsl_label,label
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def create_buttons_frame(self):
+        frame = tk.Frame()
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def run(self):
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()
